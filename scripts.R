10d08ce2adf05e5195c4c6f6a830b4d809a3cfe8
msterpa87 <marcosterpa1@gmail.com>
2021-01-31 22:06:01 +0100

    fixed graph creation

--- scripts.R
+++ scripts.R
@@ -1,18 +1,31 @@
 # correlation matrix for asd and td groups
-cor.asd <- lapply(asd_sel, cor)
-cor.td <- lapply(td_sel, cor)
+cor.asd <- lapply(asd_sel, function(x) cor(x, method = "pearson"))
+cor.td <- lapply(td_sel, function(x) cor(x, method = "pearson"))
+
+pooling.cor <- function(cor.matrices, D = 116) {
+  M <- matrix(nrow = D, ncol = D)
+  for(i in 1:D) {
+    for(j in 1:D) {
+      M[i,j] <- median(unlist(lapply(1:12, function(k) cor.matrices[[k]][i,j])))
+    }
+  }
+  return(M)
+}
+
+cor.asd.pooled <- pooling.cor(cor.asd)
+cor.td.pooled <- pooling.cor(cor.td)
 
 # combined correlations from both groups
-cor.all <- c(unlist(cor.asd), unlist(cor.td))
+cor.all <- c(unlist(cor.asd.pooled), unlist(cor.td.pooled))
 
 # threshold (80th percentile)
-t <- quantile(cor.all, probs = 0.8)
+threshold <- as.numeric(quantile(cor.all, probs = 0.8))
 
 # test for empty intersection of [-t,t] with CI(j,k)
 # TRUE iff H_0 is rejected
-int.test <- function(lower, upper, t) (-t < lower | -t > upper) & (t < lower | t > upper)
+int.test <- function(lower, upper, t) (t < lower) | (-t > upper)
 
-build.assoc.graph <- function(cor.matrices, D = 116, n = 145, m = 12) {
+build.assoc.graph <- function(cor.matrices.a, cor.matrices.b, threshold, D = 116, n = 145, m = 12) {
   # D : features, n: time series length, m: patients
   adj.matrix <- matrix(nrow = D, ncol = D)
   
@@ -24,14 +37,17 @@ build.assoc.graph <- function(cor.matrices, D = 116, n = 145, m = 12) {
     for(k in 1:D) {
       
       # vector of pearson correlation of features (j,k) for each patient
-      p <- as.numeric(unlist(lapply(cor.matrices, function(x) x[j, k])))
+      p1 <- as.numeric(unlist(lapply(cor.matrices.a, function(x) x[j, k])))
+      p2 <- as.numeric(unlist(lapply(cor.matrices.b, function(x) x[j, k])))
+      p <- abs(p1 - p2)
+      print(p)
       
       # Fisher CI for each patient corrleation coefficient
       conf.int <- lapply(p, function(x) CorCI(x, n = n, conf.level = alpha))
     
       # conf.int[i] = (rho, lower, upper)
       # bool vector of CI intersection test with threshold interval
-      int.test.results <- unlist(lapply(conf.int, function(x) int.test(x[2], x[3], t)))
+      int.test.results <- unlist(lapply(conf.int, function(x) int.test(x[2], x[3], threshold)))
       
       # edge (j,k) iff at least one H_0 is rejected
       adj.matrix[j,k] <- as.numeric(any(int.test.results))
@@ -41,7 +57,7 @@ build.assoc.graph <- function(cor.matrices, D = 116, n = 145, m = 12) {
 }
 
 # building adjacency matrix for ASD group
-asd.adj.matrix <- build.assoc.graph(cor.asd)
+asd.adj.matrix <- build.assoc.graph(cor.asd, cor.td, threshold)
 
 # simple plotting functions
 graph.asd <- graph_from_adjacency_matrix(asd.adj.matrix)
